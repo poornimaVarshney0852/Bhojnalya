9f8878eaedbea0dad77670a8b166111829aad5ad
poornimaVarshney0852 <poornima141998>
2020-07-08 04:57:04 +0530

    current location

--- Android/bhojnalya/app/src/main/java/com/example/bhojnalya/ui/home/FormRequest.java
+++ Android/bhojnalya/app/src/main/java/com/example/bhojnalya/ui/home/FormRequest.java
@@ -1,23 +1,87 @@
 package com.example.bhojnalya.ui.home;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.bhojnalya.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
 public class FormRequest extends AppCompatActivity {
-//    ImageButton imageLocation;
+    ImageView imageLocation;
+    TextView textViewLocation;
+    PlacesClient placesClient;
+
+    FusedLocationProviderClient  fusedLocationProviderClient;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.donate_form);
 
+        imageLocation = findViewById(R.id.imageView_location);
+        textViewLocation = findViewById(R.id.textView_sign_location);
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(FormRequest.this);
 
+        imageLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               if(ActivityCompat.checkSelfPermission(FormRequest.this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+                   getLocation();
+               }
+               else{
+                   ActivityCompat.requestPermissions(FormRequest.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
+               }
+            }
+
+            private void getLocation() {
+                fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        Location location = task.getResult();
+                        if(location != null){
+
+                            try {
+                                Geocoder geocoder = new Geocoder(FormRequest.this, Locale.getDefault());
+                                List<Address> addresses = geocoder.getFromLocation(
+                                        location.getLatitude(),location.getLongitude(),1);
+                                textViewLocation.setText(addresses.get(0).getAddressLine(0));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+            }
+        });
 
 
     }
+
 
 
 }
