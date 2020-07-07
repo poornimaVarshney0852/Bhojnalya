58290be0cfd21e31427704bc95e6fd0a9a59103d
poornimaVarshney0852 <poornima141998>
2020-07-08 00:00:53 +0530

    Created Navigation bar and donate form

---
+++ Android/FoodBank/app/src/main/java/com/example/foodbank/ui/home/FormRequest.java
@@ -0,0 +1,23 @@
+package com.example.foodbank.ui.home;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.foodbank.R;
+
+public class FormRequest extends AppCompatActivity {
+//    ImageButton imageLocation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.donate_form);
+
+
+
+
+
+    }
+
+
+}
